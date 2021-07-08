324eb2c9bf7364e13b221c862796bafd3a0cbc5e
Diwakar <diwakarn1599@gmail.com>
2021-07-08 12:08:18 +0530

    [Diwakar] Set values for player and computer

--- TicTacToe/PlayGame.cs
+++ TicTacToe/PlayGame.cs
@@ -23,6 +23,26 @@ namespace TicTacToe
             Console.WriteLine("Initialized Board");
             return Board;
         }
-        
+        public static char GetPlayerInput()
+        {
+            Console.WriteLine("Enter Your Character X/O");
+            char player = Convert.ToChar(Console.ReadLine());
+            return player;
+
+        }
+        public static char GetComputerValue( char playerValue)
+        {
+            char computerValue;
+            if(playerValue =='X' || playerValue == 'x')
+            {
+                computerValue = 'O';
+            }
+            else
+            {
+                computerValue = 'X';
+            }
+            return computerValue;
+
+        }
     }
 }
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -7,10 +7,15 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             char[] Board;
+            char playerValue, computerValue;
             //object for playgame class
             PlayGame game = new PlayGame();
             Console.WriteLine("Tic Tac Toe Game");
             Board = game.InitializeBoard();
+            playerValue = PlayGame.GetPlayerInput();
+            computerValue = PlayGame.GetComputerValue(playerValue);
+
+
         }
     }
 }
